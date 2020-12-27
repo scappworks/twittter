4b495416085b82f4862df786c32caadce29b3463
scott <scappworks@gmail.com>
2020-12-27 02:15:02 -0500

    remap views to use proper variables and log in password problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_081408) do
+ActiveRecord::Schema.define(version: 2020_12_27_062905) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
